3562fb62f1d466f2b4e4ee5e79fca33ab953acdd
JordanPenaloza <jordan332033@gmail.com>
2024-02-09 11:42:43 -0800

    split draw method into multiple methods to improve readability

--- app/src/main/java/com/example/subhunter/SubHunter.java
+++ app/src/main/java/com/example/subhunter/SubHunter.java
@@ -109,36 +109,42 @@ public class SubHunter extends Activity {
      */
     public void draw() {
         gameView.setImageBitmap(blankBitmap);
-
-        // Wipe the screen with a white color
         canvas.drawColor(Color.argb(255, 255, 255, 255));
-
-        // Change the paint color to black
         paint.setColor(Color.argb(255, 0, 0, 0));
 
-        // Draw the vertical lines of the grid
+        drawVerticalLines();
+        drawHorizontalLines();
+        drawPlayerShot();
+        resizeText();
+
+        Log.d("Debugging", "In draw");
+        if (debugging) {
+            printDebuggingText();
+        }
+    }
+
+    public void drawVerticalLines() {
         for(int i = 0; i < gridWidth; i++){
             canvas.drawLine(blockSize * i, 0,
                     blockSize * i, numberVerticalPixels,
                     paint);
         }
-
-        // Draw the horizontal lines of the grid
+    }
+    public void drawHorizontalLines() {
         for(int i = 0; i < gridHeight; i++){
             canvas.drawLine(0, blockSize * i,
                     numberHorizontalPixels, blockSize * i,
                     paint);
         }
-        // Draw the player's shot
+    }
+    public void drawPlayerShot() {
         canvas.drawRect(horizontalTouched * blockSize,
                 verticalTouched * blockSize,
                 (horizontalTouched * blockSize) + blockSize,
                 (verticalTouched * blockSize)+ blockSize,
                 paint );
-
-
-        // Re-size the text appropriate for the
-        // score and distance text
+    }
+    public void resizeText() {
         paint.setTextSize(blockSize * 2);
         paint.setColor(Color.argb(255, 0, 0, 255));
         canvas.drawText(
@@ -146,13 +152,6 @@ public class SubHunter extends Activity {
                         "  Distance: " + distanceFromSub,
                 blockSize, blockSize * 1.75f,
                 paint);
-
-
-
-        Log.d("Debugging", "In draw");
-        if (debugging) {
-            printDebuggingText();
-        }
     }
 
     /*
